bece4116c69f62dff0a9a4121d1f6ac6f2d63562
j2ramper <j2ramper@student.cs.uwaterloo.ca>
2019-07-21 19:24:59 -0400

    Finished front-end layout

---
+++ scripts/contact.php
@@ -0,0 +1,19 @@
+<?php
+// Get POST data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Create Message
+$formcontent="From: $name \n Message: $message";
+$recipient = "j2ramper@uwaterloo.ca";
+$subject = "Personal Site Contact Form";
+$mailheader = "From: $email \r\n";
+
+// Send message
+if( mail($recipient, $subject, $formcontent, $mailheader) ){
+  echo "Message sent!";
+}else{
+  echo "Message failed to send";
+}
+?>
